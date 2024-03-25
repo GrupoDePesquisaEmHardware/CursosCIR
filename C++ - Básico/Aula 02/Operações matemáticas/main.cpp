2f6e7b759feb0fade490b54c3e97ff73417d4ead
jorgedev <jorgericharduf@gmail.com>
2024-03-25 07:43:45 -0300

    Adicionando arquivos da segunda aula do curso de c++ básico

---
+++ C++ - Básico/Aula 02/Operações matemáticas/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x = 3;
+    int y = 2;
+
+
+    cout << x - y << endl;
+
+
+    cout << x + y << endl;
+
+
+    cout << x/y  << endl;
+
+
+    cout << x*y << endl;
+
+
+    return 0;
+}
